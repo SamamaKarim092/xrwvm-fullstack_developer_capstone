86b934770e112033a8e210907bc1b8f0a5d81ccd
Samama Karim <154582050+SamamaKarim092@users.noreply.github.com>
2026-07-29 14:27:00 +0500

    Module 3 ( 2nd lab completed )

--- server/database/app.js
+++ server/database/app.js
@@ -8,10 +8,11 @@ const port = 3030;
 app.use(cors())
 app.use(require('body-parser').urlencoded({ extended: false }));
 
-const reviews_data = JSON.parse(fs.readFileSync("reviews.json", 'utf8'));
-const dealerships_data = JSON.parse(fs.readFileSync("dealerships.json", 'utf8'));
+const reviews_data = JSON.parse(fs.readFileSync(fs.existsSync("reviews.json") ? "reviews.json" : "data/reviews.json", 'utf8'));
+const dealerships_data = JSON.parse(fs.readFileSync(fs.existsSync("dealerships.json") ? "dealerships.json" : "data/dealerships.json", 'utf8'));
 
-mongoose.connect("mongodb://mongo_db:27017/",{'dbName':'dealershipsDB'});
+const mongo_url = process.env.MONGO_URL || "mongodb://127.0.0.1:27017/dealershipsDB";
+mongoose.connect(mongo_url);
 
 
 const Reviews = require('./review');
@@ -58,17 +59,32 @@ app.get('/fetchReviews/dealer/:id', async (req, res) => {
 
 // Express route to fetch all dealerships
 app.get('/fetchDealers', async (req, res) => {
-//Write your code here
+  try {
+    const documents = await Dealerships.find();
+    res.json(documents);
+  } catch (error) {
+    res.status(500).json({ error: 'Error fetching documents' });
+  }
 });
 
 // Express route to fetch Dealers by a particular state
 app.get('/fetchDealers/:state', async (req, res) => {
-//Write your code here
+  try {
+    const documents = await Dealerships.find({ state: req.params.state });
+    res.json(documents);
+  } catch (error) {
+    res.status(500).json({ error: 'Error fetching documents' });
+  }
 });
 
 // Express route to fetch dealer by a particular id
 app.get('/fetchDealer/:id', async (req, res) => {
-//Write your code here
+  try {
+    const documents = await Dealerships.find({ id: req.params.id });
+    res.json(documents);
+  } catch (error) {
+    res.status(500).json({ error: 'Error fetching documents' });
+  }
 });
 
 //Express route to insert review
